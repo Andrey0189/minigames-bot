579479a23e74776125fa91355659ed80b8be712f
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2019-04-21 16:39:29 +0400

    Update info.js

--- Commands/info.js
+++ Commands/info.js
@@ -8,7 +8,6 @@ module.exports.run = (message) => {
     const embed = new Bot.Discord.RichEmbed()
     .setAuthor('Information about bot', message.author.avatarURL)
     .addField('Base Info', `**Servers: \`${Bot.addCommas(Bot.client.guilds.size)}\`\nChannels: \`${Bot.addCommas(Bot.client.channels.size)}\`\nUsers: \`${Bot.addCommas(Bot.client.users.size)}\`\nRAM: \`${Bot.addCommas(Math.round(process.memoryUsage().rss / 1024 / 1024 ))}/1,024 MB\`**`)
-    .addField('Our Team', `**${Bot.client.users.get(Bot.creatorID)} \`${Bot.client.users.get(Bot.creatorID).tag}\` - Main developer and designer\n${Bot.client.users.get(Bot.helperID)} \`${Bot.client.users.get(Bot.helperID).tag}\` - Helper\n${Bot.client.users.get(Bot.avatarCreatorID)} \`${Bot.client.users.get(Bot.avatarCreatorID).tag}\` - Creator of the avatar for Minigames Bot**`)
     .setTitle('Invite Bot')
     .setURL(`https://discordapp.com/oauth2/authorize?client_id=${Bot.client.user.id}&scope=bot&permissions=379904`)
     .setColor(Bot.colors.main)
