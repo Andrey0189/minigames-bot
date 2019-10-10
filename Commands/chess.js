2fa4949d56d67c14bb82d152aa9a3f739b99c96d
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2019-10-10 23:15:29 +0400

    Update chess.js

--- Commands/chess.js
+++ Commands/chess.js
@@ -2,10 +2,10 @@ module.exports.info = {
   name: 'chess',
   regex: /ch(ess)?/,
   args: '[user]',
-  desc: 'Chess',
+  desc: 'Chess (Coming Soon)',
 };
 
 module.exports.run = (message, args) => {
   const gameField = Array(64);
-  message.channel.send('ахах наебал');
+  // message.channel.send('ахах наебал');
 };
