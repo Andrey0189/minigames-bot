2df39725e237074f34fc599145a19dcd0bec6bef
Andrey0189 <39239738+Andrey0189@users.noreply.github.com>
2020-01-26 18:44:09 +0400

    Update chess.js

--- Commands/chess.js
+++ Commands/chess.js
@@ -98,13 +98,13 @@ module.exports = {
           figure = '♚';
           break;
         default: 
-          figure = ' ';
+          figure = whiteSquares.includes(i)? ' ' : '⬛';
       };
       
       return ` ${((i % 8 === 0)? i / 8 + 1 + ' ' : '') + figure + ((((i + 1) % 8 === 0) && i !== 0)? '\n' : '')}`
       });
 
-      return fieldLog.join('') + '  a b c d e f g h';
+      return fieldLog.join('') + '   a b c d e f g h';
     };
   
     piadap = (x, y) => {
